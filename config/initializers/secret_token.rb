4c9e2c5ae08604603a19192393ad75f736d25c51
Matt Strickling <mastersofevil@gmail.com>
2011-08-12 20:12:14 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pbp::Application.config.secret_token = '2bd2eae889789399cafd51b367713bb42dabf18dd294bb0edae6eb8d64e7c356a453753369ce1d49b2bb0870670f4e913bc5d53f14b339647f891fa4a1c3bb67'
